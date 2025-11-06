f8c7302f1dde870976367226b09cd305f91d2bb7
Prakash E <gopinatherg@gmail.com>
2025-11-06 14:49:00 +0530

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import SectionHeader from "./section-header";
 import { projects } from "@/lib/data";
 
